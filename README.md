6f377373363ebbb973f834a4f537998976c9670c
Hsiau-weiMu <hsiauwei@outlook.com>
2018-12-01 18:55:53 +0800

    Update version to 1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # HiddenWheelView
-A wheel view that only looks like a wheel while scrolling.
+
+A custom Android wheel view that is shown as a wheel view only when scrolling. After the scroll stops, the wheel disappears and it becomes a normal view.
+
+## Screenshot
+
+![Screenshot](./images/screenshot.gif)
+
+## Usage
+
+Add repositories to `build.gradle` of your project:
+
+```groovy
+allprojects {
+    repositories {
+        maven { url 'https://dl.bintray.com/hsiau-wei/maven' }
+    }
+}
+```
+
+Add dependencies to your app's `build.gradle` :
+
+```groovy
+dependencies {
+	compile 'me.tictok:hiddenwheelview:1.0.1'
+}
+```
+
+Put the view in your layout:
+
+```xml
+<me.tictok.hiddenwheelviewlibrary.HiddenWheelView
+        android:id="@+id/wheelView"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        app:allowVibrate="true"
+        app:itemCount="5"
+        app:itemHeight="60dp" />
+```
+
+Customize an adapter class extending `RecyclerView.Adapter` and set it as the adapter of the view:
+
+```java
+HiddenWheelView wheelView = findViewById(R.id.wheelView);
+WheelAdapter adapter = new WheelAdapter(list, R.layout.wheel_item);
+wheelView.setAdapter(adapter);
+```
+
+You can set an `OnSnapListener` if possible to listen to the snap while scrolling:
+
+```java
+wheelView.setOnSnapListener(new HiddenWheelView.OnSnapListener() {
+	@Override
+	public void onSnap(int position) {
+		// TODO
+	}
+
+	@Override
+	public void onStopScrolling() {
+		// TODO
+	}
+});
+```
+
+## Note
+
+When the attribute `android:layout_height` is set to `wrap_content` , the height of the view will be set to the product of the values of the `itemHeight` and `itemCount` attributes; but when `android:layout_height` is set to `match_parent` or a specific dimension (e.g. `400dp` ), the `itemCount` and `itemHeight` attributes will be reduntant, that is, the view will display the suitable number of items.
+
+## Changelog
+
+- 1.0.1 (Dec 01, 2018)
+  - Fix item list not filling the view when setting `android:layout_height` to `match_parent`
+- 1.0.0 (Dec 01, 2018)
+  - Initial release
+
+## License
+
+Copyright 2018 Hsiau-wei Mu
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
--- library/src/main/java/me/tictok/hiddenwheelviewlibrary/HiddenWheelView.java
+++ library/src/main/java/me/tictok/hiddenwheelviewlibrary/HiddenWheelView.java
@@ -20,11 +20,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 
 public class HiddenWheelView extends FrameLayout {
 
-    private int mHeight;
     private int mItemHeight;
     private int mItemCount;
 
@@ -39,9 +37,9 @@ public class HiddenWheelView extends FrameLayout {
 
     private CustomRecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
-    private RelativeLayout.LayoutParams mRecyclerViewParams;
-    private int mPadding;
+    private FrameLayout.LayoutParams mRecyclerViewParams;
     private CustomSnapHelper snapHelper;
+    private boolean mScrollToTop = false;
 
     private OnSnapListener mOnSnapListener;
 
@@ -52,24 +50,12 @@ public class HiddenWheelView extends FrameLayout {
     public HiddenWheelView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-
         TypedArray typedArray1 = context.obtainStyledAttributes(attrs, R.styleable.HiddenWheelView);
         mItemHeight = (int) typedArray1.getDimension(R.styleable.HiddenWheelView_itemHeight,
                 TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 0, context.getResources().getDisplayMetrics()));
         mItemCount = typedArray1.getInt(R.styleable.HiddenWheelView_itemCount, 3);
         mAllowVibrate = typedArray1.getBoolean(R.styleable.HiddenWheelView_allowVibrate, false);
         typedArray1.recycle();
-
-        int[] attrsArray = new int[]{ android.R.attr.layout_height };
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, attrsArray);
-        try {
-            mHeight = (int) typedArray.getDimension(0,
-                    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 0, context.getResources().getDisplayMetrics()));
-        } catch (UnsupportedOperationException e) {
-            mHeight = mItemCount * mItemHeight;
-        } finally {
-            typedArray.recycle();
-        }
 
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
@@ -84,8 +70,6 @@ public class HiddenWheelView extends FrameLayout {
         mRecyclerView = new CustomRecyclerView(context, attrs);
         mLayoutManager = new LinearLayoutManager(context);
         mRecyclerView.setLayoutManager(mLayoutManager);
-        mPadding = (mHeight - mItemHeight) / 2;
-        mRecyclerView.setPadding(0, mPadding, 0, mPadding);
         mRecyclerView.setClipToPadding(true);
         snapHelper = new CustomSnapHelper();
         mRecyclerView.attachSnapHelperWithListener(snapHelper, new OnSnapPositionChangeListener() {
@@ -131,7 +115,7 @@ public class HiddenWheelView extends FrameLayout {
 
             }
         }, SnapOnScrollListener.Behavior.NOTIFY_ON_SCROLL);
-        mRecyclerViewParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, mHeight);
+        mRecyclerViewParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         snapHelper.attachToRecyclerView(mRecyclerView);
         addView(mRecyclerView, mRecyclerViewParams);
     }
@@ -141,11 +125,28 @@ public class HiddenWheelView extends FrameLayout {
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        switch (getLayoutParams().height) {
+            case ViewGroup.LayoutParams.WRAP_CONTENT:
+                mRecyclerViewParams.height = mItemCount * mItemHeight;
+                mRecyclerView.setLayoutParams(mRecyclerViewParams);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
     protected boolean drawChild(Canvas canvas, View child, long drawingTime) {
         int layer = canvas.saveLayer(0, 0, getWidth(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
         boolean draw = super.drawChild(canvas, child, drawingTime);
         int padding = (mRecyclerView.getHeight() - mItemHeight) / 2;
         mRecyclerView.setPadding(0, padding, 0, padding);
+        if (!mScrollToTop) {
+            mLayoutManager.scrollToPosition(0);
+            mScrollToTop = true;
+        }
         mShaderTop = new LinearGradient(0, 0, 0, padding, 0xff000000, 0x00000000, Shader.TileMode.CLAMP);
         mPaint.setShader(mShaderTop);
         canvas.drawPaint(mPaint);
